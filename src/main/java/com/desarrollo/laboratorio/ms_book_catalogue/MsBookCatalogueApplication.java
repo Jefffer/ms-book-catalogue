85947aebebd76652a6053f0ece03e853d89d8d35
MarioTriMar <mario.t.m2001@hotmail.com>
2025-01-31 19:54:01 +0100

    commit

---
+++ src/main/java/com/desarrollo/laboratorio/ms_book_catalogue/MsBookCatalogueApplication.java
@@ -0,0 +1,13 @@
+package com.desarrollo.laboratorio.ms_book_catalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBookCatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBookCatalogueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desarrollo/laboratorio/ms_book_catalogue/MsBookCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desarrollo.laboratorio.ms_book_catalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBookCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
